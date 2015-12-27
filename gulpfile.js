9b2ed26fa14fcff8a49c874b9b7f0085530a0279
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-12-27 20:41:06 +0000

    Fix typo

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,3 @@
-// Check gulp folder for config and gulp/babel for tasks
+// Check gulp/ folder for config and gulp/tasks for tasks
 var requireDir = require('require-dir');
 requireDir('./gulp/tasks', { recurse: true });
